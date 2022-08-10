b4255e319d5cbc1b2d2885bdcaab3323a5a12e08
matews-sousa <mateussdamiano@gmail.com>
2022-08-10 17:46:13 -0300

    fix production api baseURL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://127.0.0.1:8000/api/"
-      : import.meta.env.BASE_API_URL,
+      : import.meta.env.VITE_BASE_API_URL,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
